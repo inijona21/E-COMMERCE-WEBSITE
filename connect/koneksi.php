2fde0cd0ad88ff48c1b29bfddaa2f0794fce69f3
inijona21 <128442638+inijona21@users.noreply.github.com>
2023-12-06 20:08:44 +0700

    Add files via upload

---
+++ connect/koneksi.php
@@ -0,0 +1,10 @@
+<?php
+
+  $host = "localhost"; 
+  $user = "root";
+  $pass = "";
+  $nama_db = "ujian_pweb"; //nama database
+  // Database 1
+  $con = new mysqli($host,$user,$pass,$nama_db); 
+
+?>
